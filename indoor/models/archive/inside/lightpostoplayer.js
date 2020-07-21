266ea7ad300f4bd955c8f8a00fd30a6bfdecd755
jin <32600939+madjin@users.noreply.github.com>
2020-07-20 18:08:43 -0700

    delete duplicate

--- indoor/models/archive/inside/lightpostoplayer.js
+++
@@ -1,8 +0,0 @@
-
-room.update = function(msec) 
-{	
-	room.objects["Light1"].pos.x = player.pos.x + 1;
-	room.objects["Light1"].pos.y = player.pos.y + 1;
-	room.objects["Light1"].pos.z = player.pos.z;
-	
-}
